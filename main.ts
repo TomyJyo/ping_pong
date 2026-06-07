d660752623f33f1f05df513415e3e43e5d08251c
TomyJyo <OscarEizo15@outlook.jp>
2026-06-07 18:19:58 +0900

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -38,15 +38,15 @@
  */
 input.onButtonPressed(Button.A, function () {
     customEditor.addMemo("【機能】ﾗｹｯﾄを左に1つｽﾗｲﾄﾞ")
-    customEditor.addMemo("ﾗｹｯﾄは左右2つLEDで構成し位置は左端で示す")
-    if (bar_x > 0) {
-        led.unplot(bar_x + 1, 4)
-        bar_x = bar_x - 1
-        led.plot(bar_x, 4)
+    customEditor.addMemo("ﾗｹｯﾄは左右2つのLEDで構成し位置は左端で示す。")
+    if (int_Racket_X > 0) {
+        led.unplot(int_Racket_X + 1, 4)
+        int_Racket_X = int_Racket_X - 1
+        led.plot(int_Racket_X, 4)
     }
 })
-function BGM (sound: string) {
-    if (sound == "go") {
+function BGM (strSound: string) {
+    if (strSound == "go") {
         music.play(music.tonePlayable(262, music.beat(BeatFraction.Half)), music.PlaybackMode.UntilDone)
         music.rest(music.beat(BeatFraction.Half))
         music.play(music.tonePlayable(262, music.beat(BeatFraction.Half)), music.PlaybackMode.UntilDone)
@@ -54,36 +54,36 @@ function BGM (sound: string) {
         music.play(music.tonePlayable(262, music.beat(BeatFraction.Half)), music.PlaybackMode.UntilDone)
         music.rest(music.beat(BeatFraction.Half))
         music.play(music.tonePlayable(523, music.beat(BeatFraction.Double)), music.PlaybackMode.InBackground)
-    } else if (sound == "wall") {
+    } else if (strSound == "wall") {
         music.play(music.tonePlayable(392, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
-    } else if (sound == "racket") {
+    } else if (strSound == "racket") {
         music.play(music.tonePlayable(784, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
-    } else if (sound == "over") {
+    } else if (strSound == "over") {
         music._playDefaultBackground(music.builtInPlayableMelody(Melodies.Dadadadum), music.PlaybackMode.InBackground)
     }
 }
 input.onButtonPressed(Button.B, function () {
     customEditor.addMemo("【機能】ﾗｹｯﾄを右に1つｽﾗｲﾄﾞ")
-    customEditor.addMemo("ﾗｹｯﾄは左右2つLEDで構成し位置は左端で示す")
-    if (bar_x < 3) {
-        led.unplot(bar_x, 4)
-        bar_x = bar_x + 1
-        led.plot(bar_x + 1, 4)
+    customEditor.addMemo("ﾗｹｯﾄは左右2つのLEDで構成し位置は左端で示す")
+    if (int_Racket_X < 3) {
+        led.unplot(int_Racket_X, 4)
+        int_Racket_X = int_Racket_X + 1
+        led.plot(int_Racket_X + 1, 4)
     }
 })
-let in_game = false
-let ball_dy = 0
-let ball_dx = 0
-let ball_y = 0
-let ball_x = 0
-let interval_step = 0
-let interval = 0
-let point = 0
-let bar_x = 0
+let bol_inGame = false
+let int_Point = 0
+let int_Ball_Dy = 0
+let int_Ball_Dx = 0
+let int_Ball_Y = 0
+let int_Ball_X = 0
+let int_Interv_Dt = 0
+let int_Interv = 0
+let int_Racket_X = 0
 customEditor.addMemo("【概要】ピンポン")
-customEditor.addMemo("・5×5 LED の最下段のﾗｹｯﾄをAﾎﾞﾀﾝとBﾎﾞﾀﾝを使って左右にｽﾗｲﾄﾞさせます。")
+customEditor.addMemo("・5×5 LED の最下段のﾗｹｯﾄを、ﾎﾞﾀﾝAとﾎﾞﾀﾝBを使って左右にｽﾗｲﾄﾞさせます。")
 customEditor.addMemo("・ﾗｹｯﾄﾃでﾎﾞｰﾙを跳ね返した回数がﾎﾟｲﾝﾄになります。")
-customEditor.addMemo("・2回目以降は、AﾎﾞﾀﾝとBﾎﾞﾀﾝを同時に押してｽﾀｰﾄします。")
+customEditor.addMemo("・2回目以降は、ﾎﾞﾀﾝAとﾎﾞﾀﾝBを同時に押してｹﾞｰﾑをｽﾀｰﾄします。")
 for (let index = 0; index < 3; index++) {
     basic.showLeds(`
         # # # # #
@@ -121,64 +121,62 @@ for (let index = 0; index < 3; index++) {
  * y
  */
 basic.forever(function () {
-    point = 0
     customEditor.addMemo("interval はﾎﾞｰﾙの移動間隔(ﾐﾘ秒)、step は短くする間隔(ﾐﾘ秒)で徐々に早くなります。")
-    interval = 800
-    interval_step = 10
-    customEditor.addMemo("ball_x,ball_y はｹﾞｰﾑ開始時のﾎﾞｰﾙの位置")
-    ball_x = 3
-    ball_y = 4
-    customEditor.addMemo("ball_dx,ball_dy はｹﾞｰﾑ開始時のﾎﾞｰﾙの移動方向")
-    ball_dx = -1
-    ball_dy = -1
-    customEditor.addMemo("bar_x はｹﾞｰﾑ開始時のﾗｹｯﾄの位置")
-    bar_x = 0
+    int_Interv = 800
+    int_Interv_Dt = 10
+    customEditor.addMemo("ball_x,ball_y はﾎﾞｰﾙの位置")
+    int_Ball_X = 3
+    int_Ball_Y = 4
+    customEditor.addMemo("ball_dx,ball_dy はﾎﾞｰﾙの移動方向")
+    int_Ball_Dx = -1
+    int_Ball_Dy = -1
+    customEditor.addMemo("bar_x はﾗｹｯﾄの位置")
+    int_Racket_X = 0
+    customEditor.addMemo("pointは得点")
+    int_Point = 0
     BGM("go")
     basic.showString("GO")
-    led.plot(ball_x, ball_y)
-    led.plot(bar_x, 4)
-    led.plot(bar_x + 1, 4)
-    in_game = true
+    customEditor.addMemo("ｹﾞｰﾐ開始時のﾎﾞｰﾙとﾗｹｯﾄのLED位置を点灯")
+    led.plot(int_Ball_X, int_Ball_Y)
+    led.plot(int_Racket_X, 4)
+    led.plot(int_Racket_X + 1, 4)
+    bol_inGame = true
     customEditor.addMemo("in_game でｹﾞｰﾑ中なら繰返し")
-    while (in_game) {
-        if (ball_x + ball_dx > 4) {
-            customEditor.addMemo("ﾎﾞｰﾙの次の位置が右壁なら、横の移動方向を反転させる")
+    while (bol_inGame) {
+        if (int_Ball_X + int_Ball_Dx < 0 || int_Ball_X + int_Ball_Dx > 4) {
+            customEditor.addMemo("ﾎﾞｰﾙの次の位置が左壁か右壁なら、横の移動方向を反転させる")
             BGM("wall")
-            ball_dx = ball_dx * -1
-        } else if (ball_x + ball_dx < 0) {
-            customEditor.addMemo("ﾎﾞｰﾙの次の位置が左壁なら、横の移動方向を反転させる")
-            BGM("wall")
-            ball_dx = ball_dx * -1
+            int_Ball_Dx = int_Ball_Dx * -1
         }
-        if (ball_y + ball_dy < 0) {
+        if (int_Ball_Y + int_Ball_Dy < 0) {
             customEditor.addMemo("ﾎﾞｰﾙの次の位置が上壁なら、縦の移動方向を反転させる")
             BGM("wall")
-            ball_dy = ball_dy * -1
-        } else if (ball_y + ball_dy > 3) {
-            if (led.point(ball_x + ball_dx, ball_y + ball_dy)) {
-                customEditor.addMemo("ﾎﾞｰﾙの次の位置がﾗｹｯﾄで点灯していれば当たったことになり、縦の移動方向の反転とﾎﾟｲﾝﾄをｶｳﾝﾄｱｯﾌﾟする")
+            int_Ball_Dy = int_Ball_Dy * -1
+        } else if (int_Ball_Y + int_Ball_Dy > 3) {
+            if (led.point(int_Ball_X + int_Ball_Dx, int_Ball_Y + int_Ball_Dy)) {
+                customEditor.addMemo("ﾎﾞｰﾙの次の位置がﾗｹｯﾄで点灯していれば当たることになり、縦の移動方向の反転とﾎﾟｲﾝﾄをｶｳﾝﾄｱｯﾌﾟする")
                 BGM("racket")
-                ball_dy = ball_dy * -1
-                point = point + 1
-                if (interval - interval_step >= 0) {
+                int_Ball_Dy = int_Ball_Dy * -1
+                int_Point = int_Point + 1
+                if (int_Interv - int_Interv_Dt >= 0) {
                     customEditor.addMemo("ﾗｹｯﾄでﾎﾞｰﾙを返したので、ﾎﾞｰﾙの移動間隔(ﾐﾘ秒)を短くする")
-                    interval = interval - interval_step
+                    int_Interv = int_Interv - int_Interv_Dt
                 }
             } else {
                 customEditor.addMemo("ﾎﾞｰﾙを返せなかったのでｹﾞｰﾑ終了させる")
-                in_game = false
+                bol_inGame = false
             }
         }
-        if (in_game) {
+        if (bol_inGame) {
             customEditor.addMemo("in_game でｹﾞｰﾑ中なら、ﾎﾞｰﾙの次の移動地点のLEDを点灯し、現在位置のLEDを消灯する")
-            led.plot(ball_x + ball_dx, ball_y + ball_dy)
-            led.unplot(ball_x, ball_y)
+            led.plot(int_Ball_X + int_Ball_Dx, int_Ball_Y + int_Ball_Dy)
+            led.unplot(int_Ball_X, int_Ball_Y)
             customEditor.addMemo("ﾎﾞｰﾙの現在位置の座標も更新する")
-            ball_x = ball_x + ball_dx
-            ball_y = ball_y + ball_dy
-            basic.pause(interval)
+            int_Ball_X = int_Ball_X + int_Ball_Dx
+            int_Ball_Y = int_Ball_Y + int_Ball_Dy
+            basic.pause(int_Interv)
         } else {
-            game.setScore(point)
+            game.setScore(int_Point)
             BGM("over")
             game.gameOver()
         }
